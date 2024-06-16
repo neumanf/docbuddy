30ba39d42953b351dc31ddcf0e69517939dbbcb5
neumanf <fabricionewman@protonmail.com>
2024-06-16 18:41:28 -0300

    wip

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,8 +2,9 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::error::Error;
-
-use tauri::{App, Manager};
+use tauri::{
+    App, CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem,
+};
 
 mod shortcuts;
 
@@ -14,7 +15,37 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let open = CustomMenuItem::new("open".to_string(), "Open");
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(open)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(quit);
+    let system_tray = SystemTray::new().with_menu(tray_menu);
+
     tauri::Builder::default()
+        .system_tray(system_tray)
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::LeftClick {
+                position: _,
+                size: _,
+                ..
+            } => {
+                let window = app.get_window("main").unwrap();
+                window.show().unwrap();
+            }
+            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                "quit" => {
+                    std::process::exit(0);
+                }
+                "open" => {
+                    let window = app.get_window("main").unwrap();
+                    window.show().unwrap();
+                }
+                _ => {}
+            },
+            _ => {}
+        })
         .setup(setup)
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
@@ -25,8 +56,8 @@ fn setup(app: &mut App) -> Result<(), Box<(dyn Error + 'static)>> {
     let handle = app.handle();
     let window = handle.get_window("main").unwrap();
 
-    shortcuts::update_default(app, &window);
-    shortcuts::update_close(app, window);
+    shortcuts::update_default(app, &window)?;
+    shortcuts::update_close(app, &window)?;
 
     Ok(())
 }
--- src-tauri/src/shortcuts.rs
+++ src-tauri/src/shortcuts.rs
@@ -1,22 +1,33 @@
-use tauri::{App, Window, GlobalShortcutManager};
+use std::process::Command;
 
-pub fn update_default(app: &mut App, window: &Window) {
+use tauri::{App, GlobalShortcutManager, Window};
+
+pub fn update_default(app: &mut App, window: &Window) -> Result<(), tauri::Error> {
     let mut shortcuts = app.global_shortcut_manager();
+    let window = window.clone();
 
     shortcuts.register("Alt+D", move || {
         if window.is_visible().unwrap() {
             window.hide().unwrap();
         } else {
-            window.hide().unwrap();
             window.show().unwrap();
+            Command::new("wmctrl")
+                .args(["-a", "<sth>"])
+                .output()
+                .expect("Failed to set focus");
         }
-    });
+    })?;
+
+    Ok(())
 }
 
-pub fn update_close(app: &mut App, window: Window) {
+pub fn update_close(app: &mut App, window: &Window) -> Result<(), tauri::Error> {
     let mut shortcuts = app.global_shortcut_manager();
+    let window = window.clone();
 
     shortcuts.register("Escape", move || {
         window.hide().unwrap();
-    });
+    })?;
+
+    Ok(())
 }
